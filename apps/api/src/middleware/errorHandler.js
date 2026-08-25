425533a2472ebfd5f5f5225a4c7de98e4e365383
Aakash Agrahari <aakashagrahari45@gmail.com>
2026-08-25 17:51:11 +0530

    added an error handler

---
+++ apps/api/src/middleware/errorHandler.js
@@ -0,0 +1,27 @@
+import logger from "../utils/logger.js";
+
+const errorHandler = (error, req, res, next) => {
+    logger.error(
+        {
+            err: error,
+            method: req.method,
+            path: req.originUrl
+        },
+        "Unhandled application error"
+    );
+
+    const statusCode = error.statusCode || 500;
+
+    res.status(statusCode).json({
+        success: false,
+        error: {
+            code: error.code || "INTERNAL_SERVER_ERROR",
+            message:
+                statusCode === 500
+                    ? "An unexpected error occured."
+                    : error.message
+        }
+    });
+};
+
+export default errorHandler;
