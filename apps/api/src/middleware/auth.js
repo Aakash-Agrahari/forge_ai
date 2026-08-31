3aa293f03ad3814c0ab8a23b6c26080fecc7ed54
Aakash Agrahari <aakashagrahari45@gmail.com>
2026-08-31 21:56:09 +0530

    middleware authorization created for authentication

---
+++ apps/api/src/middleware/auth.js
@@ -0,0 +1,82 @@
+import prisma from "../config/prisma.js";
+import {hashSessionToken} from "../utils/session.js";
+
+export default async function requireAuth(req, res, next){
+    try{
+        const sessionToken = req.cookies?.forgeai_session;
+
+        if(!sessionToken){
+            return res.status(401).json({
+                success: false,
+                error: {
+                    code: "UNAUTHENTICATED",
+                    message: "Authentication required"
+                }
+            });
+        }
+
+        const tokenHash = hashSessionToken(sessionToken);
+
+        const session = await prisma.session.findUnique({
+            where: {
+                tokenHash
+            },
+            include: {
+                user: {
+                    select: {
+                        id:true,
+                        name: true,
+                        email: true,
+                        createdAt: true
+                    }
+                }
+            }
+        });
+
+        if(!session){
+            return res.status(401).json({
+                success: false,
+                error: {
+                    code: "UNAUTHENTICATED",
+                    message: "Authenticated required"
+                }
+            });
+        }
+
+        if(session.revokedAt){
+            return res.status(401).json({
+                success: false,
+                error:{
+                    code: "SESSION_REVOKED",
+                    message: "Session is no longer valid"
+                }
+            });
+        }
+
+        if(session.expiresAt <= new Date()){
+            await prisma.session.update({
+                where: {
+                    id: session.id
+                },
+                data: {
+                    revokedAt: new Date()
+                }
+            });
+
+            return res.status(401).json({
+                success: false,
+                error: {
+                    code: "SESSION_EXPIRED",
+                    message: "Session has expired"
+                }
+            });
+        }
+
+        req.user = session.user;
+        req.session = session;
+
+        next();
+    } catch (error){
+        next(error);
+    }
+}
