7fa4cc21f011762477af74de68f57be0f3a7f7f5
Aakash Agrahari <aakashagrahari45@gmail.com>
2026-09-01 18:06:13 +0530

    added the rate limiter

---
+++ apps/api/src/middleware/rateLimiters.js
@@ -0,0 +1,15 @@
+import rateLimit from "express-rate-limit";
+
+export const authRateLimiter = rateLimit({
+    windowMs: 15*60*1000,
+    limit: 15,
+    standardHeaders: "draft-8",
+    legacyHeaders: false,
+    message: {
+        success: false,
+        error: {
+            code: "RATE_LIMITED",
+            message: "Too many authentication requests. Please try again later"
+        }
+    }
+})
