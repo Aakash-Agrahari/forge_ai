beaab99335bc0738303b0bb070a32b0d1a89fa06
Aakash Agrahari <aakashagrahari45@gmail.com>
2026-09-01 22:51:20 +0530

    added the project service

---
+++ apps/api/src/services/projectService.js
@@ -0,0 +1,40 @@
+import prisma from "../config/prisma.js";
+
+export async function createdProject({
+    userId,
+    name,
+    description
+}) {
+    return prisma.project.create({
+        data: {
+            name,
+            description,
+            userId
+        },
+        select: {
+            id: true,
+            name: true,
+            description: true,
+            createdAt: true,
+            updatedAt: true
+        }
+    });
+}
+
+export async function getUserProjects(userId) {
+    return prisma.project.findMany({
+        where: {
+            userId
+        },
+        orderBy: {
+            updatedAt: "desc"
+        },
+        select: {
+            id: true,
+            name: true,
+            description: true,
+            createdAt: true,
+            updatedAt: true
+        }
+    });
+}
