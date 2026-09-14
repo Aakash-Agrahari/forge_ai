8535452e13a2ac141dee43708cced39cafd5aef0
Aakash Agrahari <aakashagrahari45@gmail.com>
2026-09-14 14:52:46 +0530

    updated the server.js so that whenever we restart the model we don't manually need to discover the model again and again

--- apps/api/src/server.js
+++ apps/api/src/server.js
@@ -1,25 +1,64 @@
 import app from "./app.js";
 import env from "./config/env.js";
 import logger from "./utils/logger.js";
+import { discoverAllModels } from "./llm/discoveryManager.js";
 
-const server = app.listen(env.port, () => {
-    logger.info(
-        {
-            port: env.port,
-            environment: env.nodeEnv
-        },
-        "ForgeAI API server started"
+const startServer = async () => {
+    try {
+        const discoveryResults =
+            await discoverAllModels();
+
+        logger.info(
+            {
+                providers: discoveryResults.length,
+                successfulProviders:
+                    discoveryResults.filter(
+                        (result) =>
+                            result.status === "success"
+                    ).length
+            },
+            "LLM model discovery completed"
+        );
+    } catch (error) {
+        logger.error(
+            {
+                error: error.message
+            },
+            "LLM model discovery failed"
+        );
+    }
+
+    const server = app.listen(
+        env.port,
+        () => {
+            logger.info(
+                {
+                    port: env.port,
+                    environment: env.nodeEnv
+                },
+                "ForgeAI API server started"
+            );
+        }
     );
-});
 
-const shutdown = (signal) => {
-    logger.info({signal}, "Shutdown signal received");
+    const shutdown = (signal) => {
+        logger.info(
+            { signal },
+            "Shutdown signal received"
+        );
 
-    server.close(() => {
-        logger.info("HTTP server closed");
-        process.exit(0);
-    });
+        server.close(() => {
+            logger.info(
+                "HTTP server closed"
+            );
+
+            process.exit(0);
+        });
+    };
+
+    process.on("SIGTERM", () => shutdown("SIGTERM"));
+
+    process.on("SIGINT",() => shutdown("SIGINT"));
 };
 
-process.on("SIGTERM", () => shutdown("SIGTERM"));
-process.on("SIGINT", () => shutdown("SIGINT"));
+startServer();
