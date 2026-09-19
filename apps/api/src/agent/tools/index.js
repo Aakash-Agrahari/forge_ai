474fde6cef8133ec0fe96a2916895d65d39cb7ba
Aakash Agrahari <aakashagrahari45@gmail.com>
2026-09-19 14:58:44 +0530

    registerd write tool

--- apps/api/src/agent/tools/index.js
+++ apps/api/src/agent/tools/index.js
@@ -1,7 +1,10 @@
 import { registerTool } from "../toolRegistery.js";
 import {listFilesTool} from "./listFiles.js";
 import {readFileTool} from "./readFiles.js";
+import {writeFileTool} from "./writeFiles.js";
+
 export function registerAgentTools(){
     registerTool(listFilesTool);
     registerTool(readFileTool);
+    registerTool(writeFileTool);
 }
