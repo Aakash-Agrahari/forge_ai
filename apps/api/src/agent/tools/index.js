5c2280deb452229a5607b44eaaa86436894fbe1f
Aakash Agrahari <aakashagrahari45@gmail.com>
2026-09-19 14:36:44 +0530

    registers the read file tool

--- apps/api/src/agent/tools/index.js
+++ apps/api/src/agent/tools/index.js
@@ -1,6 +1,7 @@
 import { registerTool } from "../toolRegistery.js";
 import {listFilesTool} from "./listFiles.js";
-
+import {readFileTool} from "./readFiles.js";
 export function registerAgentTools(){
     registerTool(listFilesTool);
+    registerTool(readFileTool);
 }
