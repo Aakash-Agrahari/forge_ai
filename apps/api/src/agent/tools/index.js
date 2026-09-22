6e7a2b99ced8fe75460eead8aca3d4a0d8214425
Aakash Agrahari <aakashagrahari45@gmail.com>
2026-09-22 23:34:44 +0530

    registerd the run command

--- apps/api/src/agent/tools/index.js
+++ apps/api/src/agent/tools/index.js
@@ -3,10 +3,12 @@ import {listFilesTool} from "./listFiles.js";
 import {readFileTool} from "./readFiles.js";
 import {writeFileTool} from "./writeFiles.js";
 import {deleteFileTool} from "./deleteFiles.js";
+import {runCommandTool} from "./runCommand.js";
 
 export function registerAgentTools(){
     registerTool(listFilesTool);
     registerTool(readFileTool);
     registerTool(writeFileTool);
     registerTool(deleteFileTool);
+    registerTool(runCommandTool);
 }
